6a2cba839df3c19dbd522a36c01c974ee19e0f0c
AllEasy <harrison.santos.prestador@alleasy.com.br>
2021-07-06 23:48:26 -0300

    feat(EP-1): Adiciona e corrige entidade de usuario e seu manager

--- api/managers.py
+++ api/managers.py
@@ -1,19 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import(
-    BaseUserManager, AbstractBaseUser
-)
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.utils import timezone
 
 
 class GerenciadorUsuario(BaseUserManager):
-    def create_user(self, email, senha):
-        if not email and not senha:
-            raise ValueError('Todos os campos devem ser preenchido.')
-
+    def _create_user(self, email, senha, is_staff, is_superuser):
+        email = self.normalize_email(email)
         user = self.model(
-            email=self.normalize_email(email),
+            email, is_staff=is_staff, is_superuser=is_superuser, data_cadastro=timezone.localtime()
         )
-
         user.set_password(senha)
+        return user
+
+    def create_user(self, email, senha):
+        user = self._create_user(email, senha, is_staff=False, is_superuser=False)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, senha):
+        user = self._create_user(email, senha, is_staff=True, is_superuser=True)
         user.save(using=self._db)
 
         return user
--- api/models.py
+++ api/models.py
@@ -3,14 +3,19 @@ from django.contrib.auth.models import AbstractBaseUser
 
 from .managers import GerenciadorUsuario
 import uuid
+from django.utils import timezone
+
 # Create your models here.
 
 
 class Usuario(AbstractBaseUser):
-    email = models.EmailField('Email', max_length=255, unique=True)
-    senha = models.CharField('senha', max_length=20)
+    email = models.EmailField("Email", max_length=255, unique=True)
+    senha = models.CharField("senha", max_length=20)
+    data_cadastro = models.DateTimeField("Data de Cadastro", default=timezone.localtime())
+    pessoa_fisica = models.UUIDField(default=uuid.uuid4, editable=False)
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
     REQUIRED_FIELDS = []
 
     objects = GerenciadorUsuario()
